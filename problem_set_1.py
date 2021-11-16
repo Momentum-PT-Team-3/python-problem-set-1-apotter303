fb6023b7adf9bdc1a0e8089c9793c4acef74083c
Andy Potterfield <potterfield2005@gmail.com>
2021-11-16 17:59:33 -0600

    first commit for problem set 1

--- problem_set_1.py
+++ problem_set_1.py
@@ -1,39 +1,62 @@
-# 1. Set the variable `given_name` to the string "Addison".
 
+from typing import Concatenate
+
+
+global celsius
+
+# 1. Set the variable `given_name` to the string "Addison".
+given_name = 'Addison'
 
 # ------------------------------------------------------------------------------
 # 2. You have 20 candies that you must divide equally among 6 people. How many candies will be left over?
 # Set variables for `candies`, `people`, `left_over` to make your tests pass.
-
+candies = 20
+people = 6
+left_over = 20%6
 
 # ------------------------------------------------------------------------------
 # 3. Create a function called `greeting` that returns "Hello, <name>!",
 # where <name> is the name given as an argument to the function.
-
+def greeting(name):
+    return Concatenate('Hello, ', name, '!')
+    #return('Hello, ' + name ,'!')
 
 # ------------------------------------------------------------------------------
 # 4. Create a function called `is_odd` that, given a number, will
 # return true if the number is odd and false if it is not. An odd number is a
 # number which, when divided by 2, has a remainder of 1 or -1.
-
+def is_odd(odd):
+    if odd%2 == 1 | -1:
+#    if isodd(odd): 
+        return True
+    else:
+        return False
 
 # ------------------------------------------------------------------------------
 # 5. Create a function called `is_even` that, given a number, will
 # return true if the number is even and false if it is not. An even number is a
 # number which, when divided by 2, has a remainder of 0.
-
+def is_even(even):
+    if is_even(even):
+        return True
+    else:
+        return False
 
 # ------------------------------------------------------------------------------
 # 6. Create a function called `fahrenheit_to_celsius` that takes a
 # Fahrenheit temperature as an argument and returns the
 # temperature in Celsius.
-
+def fahrenheit_to_celsius(fahr):
+    celsius = fahr - 30 / 2
+    return celsius
 
 # ------------------------------------------------------------------------------
 # 7. Create a function called `celsius_to_fahrenheit` that takes a
 # Celsius temperature as an argument and returns the
 # temperature in Fahrenheit.
-
+def celsius_to_fahrenheit(cels):
+    fahrenheit = (cels * 1.8) + 32
+    return fahrenheit
 
 # ------------------------------------------------------------------------------
 # 8. Create a function called `fahrenheit_to_kelvin` that takes a
@@ -42,13 +65,19 @@
 # fahrenheit_to_celsius function.
 # Absolute zero (0 K) is equivalent to −273.15 C.
 # 1 degree Kelvin equals 1 degree Celsius.
-
+def fahrenheit_to_kelvin(fahr_1):
+    kelvin = celsius_to_fahrenheit + 273.15
+    return kelvin
 
 # ------------------------------------------------------------------------------
 # 9. Create a function called `lesser` that takes two numbers as
 # arguments and returns the lesser of them. This function should
 # use an if/else statement.
-
+def lesser(num_1, num_2):
+    if num_1 < num_2:
+        return num_1
+    else:
+        return num_2
 
 # ------------------------------------------------------------------------------
 # 10. Create a function called `multigreeting` that takes a name
@@ -62,6 +91,19 @@
 # eo - Saluton, <name>!
 #
 # If any other language code is used, return nothing.
+def multigreeting(name, langCode):
+
+    if langCode == "en":
+        return Concatenate('Hello, ', name, '!')
+        #return 'Hello, ' + name, + '!'
+    elif langCode == "es":
+        return Concatenate('¡Hola, ', name, '!')
+    elif langCode == "fr":
+        return Concatenate('Bonjour, ', name, '!')
+    elif langCode == "eo":
+        return Concatenate('Saluton', name, '!')
+    else:
+        return 
 
 
 # ------------------------------------------------------------------------------
@@ -91,3 +133,26 @@
 
 # Write a function called `gcd` that takes two arguments and returns the greatest
 # common divisor using the instructions above.
+
+def gcd (int_1, int_2):
+   
+    d = 0
+    while is_even(int_1, int_2) == True:
+        a = a/2
+        b = b/2
+        d = d+1
+        g= a
+    while a != b:
+        if is_even(a) == True:
+            a = a/2
+        elif is_even(b) == True:
+            b = b/2
+        elif a > b:
+            a = (a - b)/2
+        else:
+            b = (b - a)/2
+    g = a
+    return g * 2**d
+
+
+
